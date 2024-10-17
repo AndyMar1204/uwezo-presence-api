e421ef76d6e253bc1970f6bc5590368edb1a8a46
AndyMan <josueandy5@gmail.com>
2024-10-17 13:39:30 +0200

    first commit

---
+++ src/main/java/com/andy/presenceuai/PresenceUaiApplication.java
@@ -0,0 +1,13 @@
+package com.andy.presenceuai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenceUaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PresenceUaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andy/presenceuai/PresenceUaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.presenceuai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresenceUaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
